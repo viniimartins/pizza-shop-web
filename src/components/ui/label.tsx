6592cb6eb30d6b97ecb9543a38440e4a1568f9f2
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-04-25 00:08:44 -0300

    feat: create table and filters page ordes

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 import * as LabelPrimitive from '@radix-ui/react-label'
 import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
@@ -11,7 +13,7 @@ const labelVariants = cva(
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
+  VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
