cd5dc5fff19e9c8980ace492d41244593eae2793
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-05-17 23:46:36 -0300

    feat: create test order status

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -21,22 +21,34 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
-      <span className="font-medium text-muted-foreground">
+      <span data-testid="badge" className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
     </div>
